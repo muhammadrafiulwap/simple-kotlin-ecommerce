33b2d16a05792e4d156f69ba99d52fd4fe498d1d
Programmer <programmer@localhost.localdomain>
2020-01-11 06:18:21 -0800

    [Update] Server

--- app/src/main/java/com/udacoding/kotlinsimpleecommerce/Utils/Constan.kt
+++ app/src/main/java/com/udacoding/kotlinsimpleecommerce/Utils/Constan.kt
@@ -5,7 +5,10 @@ object Constan {
     /*const val BASE_URL = "http://192.168.43.90/kotlinsimpleecommerce/index.php/Api/"
     const val IMAGE_URL = "http://192.168.43.90/kotlinsimpleecommerce/images/"*/
 
-    const val IMAGE_URL = "http://192.168.10.10/kotlinsimpleecommerce/images/"
-    const val BASE_URL = "http://192.168.10.10/kotlinsimpleecommerce/index.php/Api/"
+    /*const val IMAGE_URL = "http://192.168.10.10/kotlinsimpleecommerce/images/"
+    const val BASE_URL = "http://192.168.10.10/kotlinsimpleecommerce/index.php/Api/"*/
+
+    const val IMAGE_URL = "http://onedaykotlin.udacoding.com/images/"
+    const val BASE_URL = "http://onedaykotlin.udacoding.com/index.php/Api/"
 
 }
